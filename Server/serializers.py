093d8eb5d3bbae970d66cc739c592a9659daca52
Salyiel <162126755+Salyiel@users.noreply.github.com>
2024-10-21 11:18:15 +0300

    Delete Server directory

--- Server/serializers.py
+++
@@ -1,6 +0,0 @@
-from models import User, Therapist, Appointment, Specialization
-from flask import jsonify
-
-# General serializer function to convert models into JSON format
-def serialize(model_instance):
-    return jsonify(model_instance.to_dict())
